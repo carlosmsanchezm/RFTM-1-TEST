006a405a6ec7624293fa2de2589758d725553801
carlosmsanchezm <carlos.sanchez.m.m@gmail.com>
2023-12-18 01:22:24 -0500

    test

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hard'));
+app.get('/', (req, res) => res.send('Hardnot'));
 app.listen(3000, () => console.log('Server ready'));
